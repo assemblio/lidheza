056dd8c8e2f820b9998af03bd4fbda5d917c8176
georges <georges@opendatakosovo.org>
2016-01-18 01:13:39 +0100

    minor refinement of ad tag script

--- app/mod_serve/views.py
+++ app/mod_serve/views.py
@@ -3,7 +3,8 @@ from app import mongo_utils
 
 mod_serve = Blueprint('serve', __name__)
 
-@mod_serve.route('/fetch', methods=['GET'])
-def fetch():
-    campaign = mongo_utils.get()
-    return jsonify(campaign)
+@mod_serve.route('/fetch/<width>/<height>', methods=['GET'])
+def fetch(width, height):
+    #TODO: Check that the request is coming for a valid publisher.
+    #TODO: fetch an add asset
+    return 'https://placehold.it/%sx%s' % (width, height)
